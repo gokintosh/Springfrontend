ca31a7c7a1d7dce76596a16eff823f869b0d5743
GOKUL KANNAMBALLY BALACHNADRAN <52125327+gokintosh@users.noreply.github.com>
2020-11-13 21:59:39 +0100

    initialize project frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateEmployeeComponent } from './employee/create-employee/create-employee.component';
+import { UpdateEmployeeComponent } from './employee/update-employee/update-employee.component';
+import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateEmployeeComponent,
+    UpdateEmployeeComponent,
+    EmployeeListComponent
   ],
   imports: [
     BrowserModule,
